13d3db93820119313f10107bd376e5a73aea4d27
Evernow <44484725+Evernow@users.noreply.github.com>
2022-09-30 04:06:25 -0400

    Update nvidia-updater.py

--- .github/workflows/nvidia-updater.py
+++ .github/workflows/nvidia-updater.py
@@ -18,9 +18,9 @@ if list_of_lines[0] != r'<meta HTTP-Equiv="Refresh" content="0; URL=' + res['r47
   a_file.close()
 
 import datetime
-if datetime.datetime.now().strftime("%d") == '30': # Cancels out stupid Github idle outs
-  with open("IdlerFaker.txt ", "a") as f:
-      import datetime
-      if str(datetime.datetime.now().strftime("%Y-%m-%d")) not in f:
+#if datetime.datetime.now().strftime("%d") == '30': # Cancels out stupid Github idle outs
+#  with open("IdlerFaker.txt ", "a") as f:
+     # import datetime
+      #if str(datetime.datetime.now().strftime("%Y-%m-%d")) not in f:
         #datetime.datetime.now().strftime("%Y-%m-%d")
-        f.write(str(datetime.datetime.now().strftime("%Y-%m-%d")))
+      #  f.write(str(datetime.datetime.now().strftime("%Y-%m-%d")))
